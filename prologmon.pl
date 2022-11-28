744d5479fb21e45e2a3866b05639d1fbe567343f
Pedro Augusto Dias de Lima <110076530+p3dr0-404@users.noreply.github.com>
2022-11-28 14:02:20 -0300

    Add files via upload

---
+++ prologmon.pl
@@ -0,0 +1,295 @@
+pokemonType(bulbasaur, grass, poison).
+pokemonType(ivysaur, grass, poison).
+pokemonType(venusaur, grass, poison).
+pokemonType(charmander, fire, x).
+pokemonType(charmeleon, fire, x).
+pokemonType(charizard, fire, flying).
+pokemonType(squirtle, water, x).
+pokemonType(wartortle, water, x).
+pokemonType(blastoise, water, x).
+pokemonType(caterpie, bug, x).
+pokemonType(metapod, bug, x).
+pokemonType(butterfree, bug, flying).
+pokemonType(weedle, bug, poison).
+pokemonType(kakuna, bug, poison).
+pokemonType(beedrill, bug, poison).
+pokemonType(pidgey, normal, flying).
+pokemonType(pidgeotto, normal, flying).
+pokemonType(pidgeot, normal, flying).
+pokemonType(rattata, normal, x).
+pokemonType(raticate, normal, x).
+pokemonType(spearow, normal, flying).
+pokemonType(fearow, normal, flying).
+pokemonType(ekans, poison, x).
+pokemonType(arbok, poison, x).
+pokemonType(pikachu, electric, x).
+pokemonType(raichu, electric, x).
+pokemonType(sandshrew, ground, x).
+pokemonType(sandslash, ground, x).
+pokemonType(nidoran-f, poison, x).
+pokemonType(nidorina, poison, x).
+pokemonType(nidoqueen, poison, ground).
+pokemonType(nidoran-m, poison, x).
+pokemonType(nidorino, poison, x).
+pokemonType(nidoking, poison, ground).
+pokemonType(clefairy, fairy, x).
+pokemonType(clefable, fairy, x).
+pokemonType(vulpix, fire, x).
+pokemonType(ninetales, fire, x).
+pokemonType(jigglypuff, normal, fairy).
+pokemonType(wigglytuff, normal, fairy).
+pokemonType(zubat, poison, flying).
+pokemonType(golbat, poison, flying).
+pokemonType(oddish, grass, poison).
+pokemonType(gloom, grass, poison).
+pokemonType(vileplume, grass, poison).
+pokemonType(paras, bug, grass).
+pokemonType(parasect, bug, grass).
+pokemonType(venonat, bug, poison).
+pokemonType(venomoth, bug, poison).
+pokemonType(diglett, ground, x).
+pokemonType(dugtrio, ground, x).
+pokemonType(meowth, normal, x).
+pokemonType(persian, normal, x).
+pokemonType(psyduck, water, x).
+pokemonType(golduck, water, x).
+pokemonType(mankey, fighting, x).
+pokemonType(primeape, fighting, x).
+pokemonType(growlithe, fire, x).
+pokemonType(arcanine, fire, x).
+pokemonType(poliwag, water, x).
+pokemonType(poliwhirl, water, x).
+pokemonType(poliwrath, water, fighting).
+pokemonType(abra, psychic, x).
+pokemonType(kadabra, psychic, x).
+pokemonType(alakazam, psychic, x).
+pokemonType(machop, fighting, x).
+pokemonType(machoke, fighting, x).
+pokemonType(machamp, fighting, x).
+pokemonType(bellsprout, grass, poison).
+pokemonType(weepinbell, grass, poison).
+pokemonType(victreebel, grass, poison).
+pokemonType(tentacool, water, poison).
+pokemonType(tentacruel, water, poison).
+pokemonType(geodude, rock, ground).
+pokemonType(graveler, rock, ground).
+pokemonType(golem, rock, ground).
+pokemonType(ponyta, fire, x).
+pokemonType(rapidash, fire, x).
+pokemonType(slowpoke, water, psychic).
+pokemonType(slowbro, water, psychic).
+pokemonType(magnemite, electric, steel).
+pokemonType(magneton, electric, steel).
+pokemonType(farfetchd, normal, flying).
+pokemonType(doduo, normal, flying).
+pokemonType(dodrio, normal, flying).
+pokemonType(seel, water, x).
+pokemonType(dewgong, water, ice).
+pokemonType(grimer, poison, x).
+pokemonType(muk, poison, x).
+pokemonType(shellder, water, x).
+pokemonType(cloyster, water, ice).
+pokemonType(gastly, ghost, poison).
+pokemonType(haunter, ghost, poison).
+pokemonType(gengar, ghost, poison).
+pokemonType(onix, rock, ground).
+pokemonType(drowzee, psychic, x).
+pokemonType(hypno, psychic, x).
+pokemonType(krabby, water, x).
+pokemonType(kingler, water, x).
+pokemonType(voltorb, electric, x).
+pokemonType(electrode, electric, x).
+pokemonType(exeggcute, grass, psychic).
+pokemonType(exeggutor, grass, psychic).
+pokemonType(cubone, ground, x).
+pokemonType(marowak, ground, x).
+pokemonType(hitmonlee, fighting, x).
+pokemonType(hitmonchan, fighting, x).
+pokemonType(lickitung, normal, x).
+pokemonType(koffing, poison, x).
+pokemonType(weezing, poison, x).
+pokemonType(rhyhorn, ground, rock).
+pokemonType(rhydon, ground, rock).
+pokemonType(chansey, normal, x).
+pokemonType(tangela, grass, x).
+pokemonType(kangaskhan, normal, x).
+pokemonType(horsea, water, x).
+pokemonType(seadra, water, x).
+pokemonType(goldeen, water, x).
+pokemonType(seaking, water, x).
+pokemonType(staryu, water, x).
+pokemonType(starmie, water, psychic).
+pokemonType(mr-mime, psychic, fairy).
+pokemonType(scyther, bug, flying).
+pokemonType(jynx, ice, psychic).
+pokemonType(electabuzz, electric, x).
+pokemonType(magmar, fire, x).
+pokemonType(pinsir, bug, x).
+pokemonType(tauros, normal, x).
+pokemonType(magikarp, water, x).
+pokemonType(gyarados, water, flying).
+pokemonType(lapras, water, ice).
+pokemonType(ditto, normal, x).
+pokemonType(eevee, normal, x).
+pokemonType(vaporeon, water, x).
+pokemonType(jolteon, electric, x).
+pokemonType(flareon, fire, x).
+pokemonType(porygon, normal, x).
+pokemonType(omanyte, rock, water).
+pokemonType(omastar, rock, water).
+pokemonType(kabuto, rock, water).
+pokemonType(kabutops, rock, water).
+pokemonType(aerodactyl, rock, flying).
+pokemonType(snorlax, normal, x).
+pokemonType(articuno, ice, flying).
+pokemonType(zapdos, electric, flying).
+pokemonType(moltres, fire, flying).
+pokemonType(dratini, dragon, x).
+pokemonType(dragonair, dragon, x).
+pokemonType(dragonite, dragon, flying).
+pokemonType(mewtwo, psychic, x).
+pokemonType(mew, psychic, x).
+
+fraqueza(normal, fighting).
+fraqueza(fighting, flying).
+fraqueza(fighting, psychic).
+fraqueza(fighting, fairy).
+fraqueza(flying, rock).
+fraqueza(flying, electric).
+fraqueza(flying, ice).
+fraqueza(poison, ground).
+fraqueza(poison, psychic).
+fraqueza(ground, water).
+fraqueza(ground, grass).
+fraqueza(ground, ice).
+fraqueza(rock, fighting).
+fraqueza(rock, ground).
+fraqueza(rock, steel).
+fraqueza(rock, water).
+fraqueza(rock, grass).
+fraqueza(bug, flying).
+fraqueza(bug, rock).
+fraqueza(bug,  fire).
+fraqueza(ghost, ghost).
+fraqueza(ghost, dark).
+fraqueza(steel, fighting).
+fraqueza(steel, ground).
+fraqueza(steel, fire).
+fraqueza(fire, ground).
+fraqueza(fire, rock).
+fraqueza(fire, water).
+fraqueza(water, grass).
+fraqueza(water, electric).
+fraqueza(grass, flying).
+fraqueza(grass, poison).
+fraqueza(grass, bug).
+fraqueza(grass, fire).
+fraqueza(grass, ice).
+fraqueza(electric, ground).
+fraqueza(psychic, bug).
+fraqueza(psychic, ghost).
+fraqueza(psychic, dark).
+fraqueza(ice, fighting).
+fraqueza(ice, rock).
+fraqueza(ice, steel).
+fraqueza(ice, fire).
+fraqueza(dragon, ice).
+fraqueza(dragon, dragon).
+fraqueza(dragon, fairy).
+fraqueza(fairy, poison).
+fraqueza(fairy, steel).
+fraqueza(dark, fighting).
+fraqueza(dark, bug).
+fraqueza(dark, fairy).
+
+resistente(normal, ghost).
+resistente(fighting, rock).
+resistente(fighting, dark).
+resistente(fighting, bug).
+resistente(flying, fighting).
+resistente(flying, ground).
+resistente(flying, bug).
+resistente(flying, grass).
+resistente(poison, fighting).
+resistente(poison, poison).
+resistente(poison, grass).
+resistente(poison, fairy).
+resistente(ground, poison).
+resistente(ground, rock).
+resistente(ground, electric).
+resistente(rock, normal).
+resistente(rock, flying).
+resistente(rock, poison).
+resistente(rock, fire).
+resistente(bug, fighting).
+resistente(bug, ground).
+resistente(bug, grass).
+resistente(ghost, normal).
+resistente(ghost, fighting).
+resistente(ghost, poison).
+resistente(ghost, bug).
+resistente(steel, normal).
+resistente(steel, flying).
+resistente(steel, poison).
+resistente(steel, rock).
+resistente(steel, bug).
+resistente(steel, steel).
+resistente(steel, grass).
+resistente(steel, psychic).
+resistente(steel,ice).
+resistente(steel, dragon).
+resistente(steel, fairy).
+resistente(fire, bug).
+resistente(fire, steel).
+resistente(fire, fire).
+resistente(fire, grass).
+resistente(fire, ice).
+resistente(water, steel).
+resistente(water, fire).
+resistente(water, water).
+resistente(water, ice).
+resistente(grass, ground).
+resistente(grass, water).
+resistente(grass, grass).
+resistente(grass, electric).
+resistente(electric, flying).
+resistente(electric, steel).
+resistente(electric, electric).
+resistente(psychic, fighting).
+resistente(psychic, psychic).
+resistente(ice, ice).
+resistente(dragon, fire).
+resistente(dragon, water).
+resistente(dragon, grass).
+resistente(dragon, electric).
+resistente(fairy, fighting).
+resistente(fairy, bug).
+resistente(fairy, dragon).
+resistente(fairy, dark).
+resistente(dark, ghost).
+resistente(dark, psychic).
+resistente(dark, dark).
+
+% pokemonTipos(X, Y, Z) :-
+  %  pokemonType(X, Y),
+   % pokemonType(X, Z),
+   % Z \== Y.
+
+fracoContra(P, T) :-
+    pokemonType(P, X, Y),
+    (fraqueza(X, T); fraqueza(Y, T)),
+    \+ resistente(X, T),
+    \+ resistente(Y, T).
+
+forteContra(P, T) :-
+    pokemonType(P, X, Y),
+    (resistente(X, T); resistente(Y, T)),
+    \+ fraqueza(X, T),
+    \+ fraqueza(Y, T).
+
+% X é o vencedor
+
+batalha(X, Y) :-
+    pokemonType(X, A, B),
+    ((fracoContra(Y, A), \+ forteContra(Y, B));
+    (fracoContra(Y, B), \+ forteContra(Y, A))).
